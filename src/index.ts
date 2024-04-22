f6a783101673ad5144aade98d9f9635f5f845ca0
hujinbin <437044048@qq.com>
2024-04-22 19:52:34 +0800

    feat: 水波实例和动画

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { Scene, PerspectiveCamera , WebGLRenderer,PlaneGeometry} from "three";
+import { Scene, PerspectiveCamera , 
+    WebGLRenderer,PlaneGeometry,
+    TextureLoader, RepeatWrapping,
+    Vector3
+} from "three";
 
-import { Water } from './Objects/water'
+import { Water } from 'three/examples/jsm/objects/Water.js';
 
 export const scene = new Scene()
 export const camera = new PerspectiveCamera(
@@ -12,17 +16,43 @@ export const camera = new PerspectiveCamera(
 
 console.log(22222222)
 
+const waterGeometry = new PlaneGeometry(10000, 10000);
+
+const water = new Water(
+    waterGeometry,
+    {
+        textureWidth: 512,
+        textureHeight: 512,
+        waterNormals: new TextureLoader().load('./waternormals.jpg', function (texture) {
+            texture.wrapS = texture.wrapT = RepeatWrapping;
+        }),
+        sunDirection: new Vector3(),
+        sunColor: 0xffffff,
+        waterColor: 0x001e0f,
+        distortionScale: 3.7, 
+    }
+);
+water.rotation.x = - Math.PI / 2;
+
 let renderer: WebGLRenderer;
+
+
+
+function render() {
+    water.material.uniforms["time"].value += 1.0 / 60.0;
+    requestAnimationFrame(render)
+}
+render()
 
 async function init() {
     renderer = new WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    scene.add(water);
     document.body.appendChild(renderer.domElement);
+    render()
+    console.log(water)
+    scene.add(water);
 }
-
-const waterGeometry = new PlaneGeometry(10000, 10000);
-
-const water = new Water(waterGeometry)
 
 const animate = () => {
     requestAnimationFrame(animate);
